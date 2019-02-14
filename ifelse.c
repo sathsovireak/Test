11c8c777352af241b43a1af3ebe553faa14ad150
sathsovireak <sathsovireak18@kit.edu.kh>
2019-02-14 13:06:48 +0700

    file version of ifelse

---
+++ ifelse.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int num;
+	printf("Enter a numbers");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		printf("this number is even");
+	}
+	else
+	{
+		printf("this number is odd");
+	}
+}
